80185993e97360b4869e05d580edbe71344e1be7
Javier Cornejo <Cornejo859@gmail.com>
2023-10-05 15:05:22 -0300

    prueba
    subida de archivo

---
+++ BravoIzquierdoCore/Pages/Index_DHR.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BravoIzquierdoCore.Pages
+{
+    public class Index_DHRModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BravoIzquierdoCore/Pages/Login_DHR.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BravoIzquierdoCore.Pages
+{
+    public class Login_DHRModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
